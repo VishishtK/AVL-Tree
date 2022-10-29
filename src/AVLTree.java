4b77ec19e9c96021bbd933c6c3e64b95c558bb78
vishishtK <vishisht.khilariwal@gmail.com>
2022-10-29 00:52:50 -0400

    basic insert done

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -13,6 +13,7 @@ public class AVLTree {
     }
 
     public void Insert(int key) {
+        insert(root, new Node(key));
 
     }
 
@@ -28,7 +29,9 @@ public class AVLTree {
         return null;
     }
 
-    private void insert(Node treeNode, Node newNode) {
+    // Inserts the new Node and returns a node with an imbalance if any
+    private Node insert(Node treeNode, Node newNode) {
+        // Recursively go down the tree to the leaf and add the node
         if (newNode.value <= treeNode.value) {
             if (treeNode.leftChild == null)
                 treeNode.leftChild = newNode;
@@ -40,5 +43,24 @@ public class AVLTree {
             else
                 insert(treeNode.rightChild, newNode);
         }
+
+        // Update height of the node
+        if (treeNode.leftChild == null && treeNode.rightChild == null)
+            treeNode.height = 1;
+        else if (treeNode.leftChild == null)
+            treeNode.height = 1 + treeNode.rightChild.height;
+        else if (treeNode.rightChild == null)
+            treeNode.height = 1 + treeNode.leftChild.height;
+        else
+            treeNode.height = 1 + Math.max(treeNode.leftChild.height, treeNode.rightChild.height);
     }
+
+    private void leftRotate(Node node){
+
+    }
+
+    private void rightRotate(Node node){
+
+    }
+
 }
--- src/Node.java
+++ src/Node.java
@@ -1,17 +1,19 @@
-public class Node{
+public class Node {
     int value;
+    int height;
     Node leftChild;
     Node rightChild;
 
-    public Node(){
+    public Node() {
 
     }
 
-    public Node(int value){
+    public Node(int value) {
         this.value = value;
+        this.height = 0;
     }
 
-    public Node(int value, Node left, Node right){
+    public Node(int value, Node left, Node right) {
         this.value = value;
         this.leftChild = left;
         this.rightChild = right;
