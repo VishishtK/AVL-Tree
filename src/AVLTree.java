72faff583f100c0aa72d9030044706b78a8fe075
vishishtK <vishisht.khilariwal@gmail.com>
2022-11-07 01:53:59 -0500

    need to fix

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -110,4 +110,8 @@ public class AVLTree {
         return y;
     }
 
+    public void PrintTree() {
+        
+    }
+
 }
--- src/App.java
+++ src/App.java
@@ -3,17 +3,41 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class App {
+
+    public static AVLTree avlTree = null;
+
+    public static void ProcessInput(String input) {
+        String functionName = input.substring(0, input.indexOf("("));
+        String[] params = input.substring(input.indexOf("(") + 1, input.indexOf(")")).split(",");
+        switch (functionName) {
+            case "Initialize":
+                avlTree = new AVLTree();
+                break;
+            case "Insert":
+                avlTree.Insert(Integer.parseInt(params[0]));
+                break;
+            case "Delete":
+                avlTree.Delete(Integer.parseInt(params[0]));
+                break;
+            case "Search":
+                if (params.length == 2)
+                    avlTree.Search(Integer.parseInt(params[0]), Integer.parseInt(params[1]));
+                else
+                    avlTree.Search(Integer.parseInt(params[0]));
+                break;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        AVLTree avlTree = new AVLTree();
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader("input.txt"));
             String line = reader.readLine();
             while (line != null) {
-                System.out.println(line);
-                // read next line
+                ProcessInput(line);
                 line = reader.readLine();
             }
+            avlTree.PrintTree(avlTree.root, "", true);
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
